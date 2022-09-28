ba818199575d1bb78448481248bad18f4cb8f70b
JuanjoArce <juanjosearceherrera@gmail.com>
2022-09-27 23:30:56 -0300

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { storiesOf } from '@storybook/react';
+import Button from './components/Button';
+import { action } from '@storybook/addon-actions';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+storiesOf('Button', module)
+  .add('Primary', ()=> (
+     <Button 
+       buttonType='primary'
+     onClick={action('click')}>
+     Obten Premium
+     </Button>
+))
